8a8760bf016c4b4741d5526a40eb546fd2d8f26d
Alcaide F. Inocente <ialcaidef@hotmail.com>
2020-10-02 07:14:50 +0200

    Add project files.

---
+++ ContosoConf/scripts/pages/schedule.js
@@ -0,0 +1,14 @@
+﻿/// <reference path="../HtmlTemplate.js" />
+/// <reference path="../LocalStarStorage.js" />
+/// <reference path="../datetime.js" />
+import { ScheduleList } from "../ScheduleList.js";
+import { LocalStarStorage } from "../LocalStarStorage.js";
+
+const scheduleListElement = document.getElementById("schedule");
+const scheduleList = new ScheduleList(
+    scheduleListElement,
+    new LocalStarStorage(localStorage)
+);
+
+scheduleList.startDownload();
+
